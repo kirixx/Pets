c2469cecc4cc5a04ff245ce2a5fce279b630f221
Kirill <ksfonmu@gmail.com>
2021-11-04 14:49:46 +0200

    template class vs typename *just for practice*

--- Pets_solution/MayersRules/src/main.cpp
+++ Pets_solution/MayersRules/src/main.cpp
@@ -2,6 +2,36 @@
 #include "CompleteModernC++/Printer.h"
 #include "CompleteModernC++/Deleter.h"
 #include "CompleteModernC++/StringPractice.h"
+#include <vector>
+
+namespace TemplateTest
+{
+    //the example when class and typename is not interchangeably
+    template <typename T>
+    struct wrapper
+    {
+        using value_type = T;
+        value_type value;
+    };
+
+    template <typename T>
+    struct foo
+    {
+        T wrapped_value;
+        //you need to return a wrapped value, class is not allowed here.
+        typename T::value_type get_wrapped_value() { return wrapped_value.value; }
+    };
+
+    //template of template. since c++17 class could be changed on typename.
+    template < template < typename, typename > /*here*/class Container, typename Type >
+    class Example
+    {
+        Container< Type, std::allocator < Type > > baz;
+    };
+
+}
+
+
 
 int main()
 {
@@ -22,6 +52,12 @@ int main()
     std::string patt("BOOM");
 
     size_t pos = StringPractice::Find(src, patt);
+
+
+    TemplateTest::foo<TemplateTest::wrapper<int>> f{ {42} };
+    std::cout << f.get_wrapped_value() << '\n';
+
+    TemplateTest::Example<std::vector, int> a;
 
     return 0;
 }
